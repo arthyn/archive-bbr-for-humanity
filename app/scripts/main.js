e89d2d7ee99ccc323b918ca8cc9c07dbe611acdd
Hunter Miller <hmiller@bbrcreative.com>
2014-12-02 16:55:16 -0600

    been lazy with commits, nearly finished with layout and functionality. Next thing to add is sharing.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,15 @@
-var cardPhrases = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+var cardPhrases = ['Never change.', 'Live every day like Facebook is down.', 'Naughty / Nice', 'Wow.<br>You nailed it.', 'If real life were Yelp, I\'d give you: <span class="ss-star"></span><span class="ss-star"></span><span class="ss-star"></span><span class="ss-star"></span><span class="ss-star"></span>', 'Surprise your friends by using your phone as a phone.', 'Be the wo/man you want to Google.', 'You must be butter \'cause you\'re on a roll', 'Tell a millenial s/he\'s special, because Lord knows nobody else has.', 'How about instead, we pray the hate away?', 'DO THE HUSTLE! Do-do-do-da-do-da-do-do-do...', 'When life hands you Nickelback, put vodka in your lemonade.', 'Don\'t sweat the small stuff, unless you\'re eating a jalapeño popper or something.', 'Hello. You\'re amazing. That is all.', '<span class="ss-like"></span>', '*slow clap*', 'You\'re the Who to my Dat.', 'You. Me. Mistletoe.', 'Awesomeville<br>Population:<br>You', '#NicelyDone', 'Dance like the NSA isn\'t watching.', 'Use your fancy text machine to call your parents.', '4 out of 5 dentists agree: Nice legs.', 'Killer fedora,<br>bro.', 'You\'re so cool, I\'m at a loss for emojis', 'Before you reply, ask, "What would Morgan Freeman do?"', 'I went ahead and cleared your browser history. You\'re welcome.', 'Spend as much time worrying as you do reading the iTunes user agreement.', 'This card entitles the recipient to one sarcasm-free conversation. Or does it?', 'Go Kanye West, young man. Go Kanye West.', 'I admit I was wrong-ish.', 'Thank you for being the anti-douchebag.', 'Less whining. More wine-ing.', 'H8Rs gonna H8.', 'You rock.', 'You make it look easy.', '¡Bueno!', 'You do you.', 'Zing!', 'Your skills make me question my own life choices.', 'Bruce Springsteen + Tony Danza. Still not as boss as you.'];
 var homeCard;
+var smallHomeCard;
 var infoCard;
 var ideaCard;
 var diyCard;
 var ecardsCard;
 var cardSlots;
+var gutterWidth = 3;
+var footerHeight = 35;
+var cardWidth;
+var cardHeight;
 var outputCards = {};
 
 function getRandomInt(min, max) {
@@ -27,13 +32,11 @@ var card = function (spec) {
 	};
 
 	function generateUniquePosition(cards, potentialSlots) {
-		var random = getRandomInt(2, potentialSlots+2);
-		if (cards[random]) {
-			var position = generateUniquePosition(cards, potentialSlots);
-			return position;
-		} else {
-			return random;
+		var random = getRandomInt(2, potentialSlots + 1);
+		while(cards[random]){
+			random = getRandomInt(2, potentialSlots + 1);
 		}
+		return random;
 	}
 
 	return that;
@@ -43,10 +46,12 @@ function generateCardHTML(phrase) {
 	return '<p>' + phrase + '</p>';
 }
 function initializeCards() {
-	homeCard = card({html: '<h2><span class="green">Holiday</span><br> Cards<br> for<br> Humanity</h2>', position: 1, locked: true});
-	infoCard = card({html: '<p>info</p>', position: 2});
-	ideaCard = card({html: '<h2>Ideas for Humanity</h2>', position: 3});
-	diyCard = card({html: '<h2>DIY for Humanity</h2>', position: 4});
+	smallHomeCard = card({html: '<h1><span class="green">Holiday</span><br> Cards<br> for<br> Humanity</h1><p class="swipe-more">swipe for more <span class="ss-right"></span></p>', position: 1, locked: true});
+	homeCard = card({html: '<h1><span class="green">Holiday</span><br> Cards<br> for<br> Humanity</h1>', position: 1, locked: true});
+	infoCard = card({html: '<h3>Our gift to you is your gift to give...</h3><p class="copy">for a job well done or to butter up your boss. Give props to your peeps, or just make someone\'s day a little brighter. Deck the halls and plaster friends\' walls by sending an e-card. Or print out your own stack and, heck, stock some stalls.</p><br><p class="copy">From the heart is where it\'s at&#8211;so spread some <span class="goodness">good for goodness\' sake.</span><br><div id="shuffle-button"><p><span class="ss-shuffle"></span> shuffle cards</p></div>', position: 2});
+	ideaCard = card({html: '<h2>Ideas<br>For Humanity</h2>', position: 3});
+	diyCard = card({html: '<h2>DIY<br>For Humanity</h2>', position: 4});
+	//<img src="images/shuffle.png" alt="shuffle">
 	//ecardsCard = card({html: '<h2>Ecards for Humanity</h2>', position: 4});
 }
 
@@ -55,9 +60,11 @@ function generateRandomCards(smallLayout) {
 	var numCardOptions = cardPhrases.length;
 	var outputCards = {};
 
-	outputCards[homeCard.getPosition()] = homeCard;
+
+	outputCards[smallHomeCard.getPosition()] = smallHomeCard;
 	//outputCards[ecardsCard.getPosition()] = ecardsCard;
 	if(!smallLayout) {
+		outputCards[homeCard.getPosition()] = homeCard;
 		outputCards[infoCard.getPosition()] = infoCard;
 		outputCards[ideaCard.getPosition()] = ideaCard;
 		outputCards[diyCard.getPosition()] = diyCard;
@@ -71,10 +78,11 @@ function generateRandomCards(smallLayout) {
 			random = getRandomInt(0,numCardOptions);
 		}
 		var phrase = cardPhrases[random];
+		console.log("Phrase: " + phrase + " Position: " + random);
 		cardChoices[random] = phrase;
-		var newCard = card({html: generateCardHTML(phrase)});
+		var newCard = card({html: generateCardHTML(phrase), position: smallLayout ? i + 2 : i + 5});
 		console.log(newCard.getHTML());
-		newCard.changePosition(outputCards, cardSlots);
+		//newCard.changePosition(outputCards, cardSlots);
 		console.log(newCard.getPosition());
 		outputCards[newCard.getPosition()] = newCard;
 	}
@@ -83,37 +91,68 @@ function generateRandomCards(smallLayout) {
 	return outputCards;
 }
 
-
+function convertCardToCanvas() {
+	var canvas = document.createElement('canvas');
+	canvas.id = 'card-canvas';
+	var context = canvas.getContext('2d');
+}
 
 function setupSmallLayout() {
-	outputCards['pointer'] = 1;
 	var cards = $('.card');
 	var cardWidth = $(cards[0]).width();
+	var cardHeight = $(cards[0]).height();
 	var gutterWidth = cardWidth / 8;
 
 	for(var i=1; i<cards.length; i++) {
+		$(cards[i]).toggleClass('animate');
 		$(cards[i]).css('position', 'absolute')
-				.css('left', '-' + ((cardWidth + gutterWidth) * i) + 'px')
-				.css('top', '0');
+				   .css('top', '35px')
+				   .css('height', cardHeight + 'px');
+		if(i === 3) {
+		 	$(cards[i]).css('left', cardWidth + gutterWidth + 'px');
+		 			//.css('display', 'none');
+		}else{
+			$(cards[i]).css('left', '-' + ((cardWidth + gutterWidth) * i) + 'px');
+		}
+		$(cards[i]).toggleClass('animate');
 	}
 }
 
 function insertCardHTML(source, smallLayout) {
 	var cards = $('.card .face');
-
+	var backs = $('.card .back');
 	for(var i = 0; i<cards.length; i++) {
 		$(cards[i]).empty().append(source[i+1].getHTML());
+		$(backs[i]).empty().append('<h2>Share this: </h2><div class="social"><span class="st_facebook_large" displayText="Facebook"></span><span class="st_twitter_large" displayText="Tweet"></span><span class="st_pinterest_large" displayText="Pinterest"></span></div>');
 	}
 }
 function bindClickFlip() {
 	$('.card').on('click', function() {
-		var currentStyles = $(this).attr('style');
-		var rotated = /.*\srotateY\(180deg\);\s.*/;
-		console.log(rotated.test(' rotateY(180deg); '));
-		if(currentStyles.match(rotated)) {
-			var newStyles = currentStyles.replace('scale3d(1, 1, 1) rotateY(180deg);', 'scale3d(1, 1, 1) rotateY(20deg);');
+		var nop3d = $('html').hasClass('no-transformstylepreserve3d');
+		var currentStyles = nop3d ? $(this).children('.back').css('transform') : $(this).attr('style');
+		var rotated = /.*\s-*rotateY\(180deg\);\s.*/;
+		console.log(rotated.test(currentStyles));
+		console.log(currentStyles);
+		if(currentStyles.match(rotated) || currentStyles == 'matrix3d(-1, 0, 0, 0, 0, 1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1)') {
+			if(nop3d) {
+				$(this).children('.face').css('display', 'none');
+				$(this).children('.back').css('transform', 'rotateY(0deg)');
+				console.log("no preserve-3d");
+			}
+			else {
+				var newStyles = currentStyles.replace('scale3d(1, 1, 1) rotateY(180deg);', 'scale3d(1, 1, 1) rotateY(20deg);');
+				console.log("preserve-3d");
+			}
 		} else {
-			var newStyles = currentStyles.replace('scale3d(1, 1, 1) rotateY(20deg);', 'scale3d(1, 1, 1) rotateY(180deg);');
+			if(nop3d) {
+				$(this).children('face').css('display', 'block');
+				$(this).children('back').css('transform', 'rotateY(-180deg)');
+				console.log("no preserve-3d");
+			}
+			else {
+				var newStyles = currentStyles.replace('scale3d(1, 1, 1) rotateY(20deg);', 'scale3d(1, 1, 1) rotateY(180deg);');
+				console.log("preserve-3d");
+			}
 		}
 
 		$(this).attr('style', newStyles);
@@ -146,6 +185,10 @@ function setupHammer(element) {
 
 		var START_X = Math.round((area.offsetWidth - el.offsetWidth) / 2);
 		var START_Y = Math.round((area.offsetHeight - el.offsetHeight) / 2);
+
+		var cards = $('.card');
+		var cardWidth = $(cards[0]).width();
+		var gutterWidth = cardWidth / 8;
 
 		var ticking = false;
 		var transform;
@@ -211,22 +254,58 @@ function setupHammer(element) {
 
 		function onSwipe(ev) {
 		    
-		    
-		    var oldCard = el;
-		    el.id = '';
-		    var newActive = document.getElementsByClassName('shuffle-item')[1];
-		    newActive.id = 'active-card';
-		    setupHammer(newActive);
-		    newActive.style.position = 'relative';
-		    newActive.style.left = '0';
-		    var cardList = document.querySelector('#card-list');
-		    oldCard.className = "card shuffle-item";
-		    oldCard = cardList.removeChild(oldCard);
-		    cardList.appendChild(oldCard);
-		    oldCard.removeAttribute('style');
-		    setupSmallLayout();
-
-		    
+		    if(outputCards.pointer !== Object.keys(outputCards).length - 1 && ev.direction & Hammer.DIRECTION_RIGHT) {
+			    el.id = '';
+			    var cards = document.getElementsByClassName('shuffle-item');
+			    var newActive = cards[1];
+			    newActive.id = 'active-card';
+			    newActive.style.position = 'relative';
+			    newActive.style.left = '0';
+			    newActive.style.top = '0';
+			    var cardList = document.querySelector('#card-list');
+			    var oldCard = cards[0];
+			    oldCard.className = "card shuffle-item";
+			    oldCard = cardList.removeChild(oldCard);
+			    cardList.appendChild(oldCard);
+			    var lastCard = document.querySelectorAll('.card .face')[2];
+			    if(outputCards.pointer > 1  && outputCards.pointer < (Object.keys(outputCards).length - 3)) {
+			    	lastCard.innerHTML = outputCards[outputCards.pointer + 3].getHTML();
+			    	console.log(lastCard.innerHTML);
+			    }
+			    console.log(Object.keys(outputCards).length - 3);
+			    if(outputCards.pointer < Object.keys(outputCards).length - 1) {
+			    	outputCards.pointer += 1;
+			    }
+			    console.log(outputCards.pointer);
+			    setupSmallLayout();
+			}
+			else if(outputCards.pointer !== 1 && ev.direction & Hammer.DIRECTION_LEFT) {
+				el.id = '';
+				var cards = document.getElementsByClassName('shuffle-item');
+				var cardList = document.querySelector('#card-list');
+				var newActive = cardList.removeChild(cards[3]);
+				newActive.id = 'active-card';
+				newActive.style.position = 'relative';
+				newActive.style.left = '0';
+				newActive.style.top = '0';
+				cardList.insertBefore(newActive, cards[0]);
+				var nextCard = document.querySelectorAll('.card .face')[3];
+				if(outputCards.pointer > 2  && outputCards.pointer < (Object.keys(outputCards).length - 1)) {
+					nextCard.innerHTML = outputCards[outputCards.pointer - 2].getHTML();
+					console.log(nextCard.innerHTML);
+				}
+				console.log(Object.keys(outputCards).length - 3);
+				if(outputCards.pointer > 1) {
+					outputCards.pointer -= 1;
+				}
+				console.log(outputCards.pointer);
+				setupSmallLayout();
+			}
+			
+			clearTimeout(timer);
+		    timer = setTimeout(function () {
+		        resetElement();
+		    }, 600);
 
     	    logEvent(ev);
     	    requestElementUpdate();
@@ -271,8 +350,8 @@ window.Shuffler = (function ($) {
 			itemSelector: '.shuffle-item',
 			speed: 400,
 			easing: 'ease',
-			columnWidth: 150,
-			gutterWidth: 2
+			columnWidth: cardWidth,
+			gutterWidth: gutterWidth
 		});
 
 		this.shuffle = this.$el.data('shuffle');
@@ -299,7 +378,82 @@ window.Shuffler = (function ($) {
 
 }(jQuery));
 
-function populateLargeLayout() {};
+Modernizr.addValueTest = function(property,value){
+    var testName= (property+value).replace(/-/g,'');
+    Modernizr.addTest(testName , function () {
+        var element = document.createElement('link');
+        var body = document.getElementsByTagName('HEAD')[0];
+        var properties = [];
+        var upcaseProp = property.replace(/(^|-)([a-z])/g, function(a, b, c){ return c.toUpperCase(); });
+        properties[property] = property;
+        properties['Webkit'+upcaseProp] ='-webkit-'+property;
+        properties['Moz'+upcaseProp] ='-moz-'+property;
+        properties['ms'+upcaseProp] ='-ms-'+property;
+
+        body.insertBefore(element, null);
+        for (var i in properties) {
+            if (element.style[i] !== undefined) {
+                element.style[i] = value;
+            }
+        }
+        //ie7,ie8 doesnt support getComputedStyle
+        //so this is the implementation
+        if(!window.getComputedStyle) {
+            window.getComputedStyle = function(el, pseudo) {
+                this.el = el;
+                this.getPropertyValue = function(prop) {
+                    var re = /(\-([a-z]){1})/g;
+                    if (prop == 'float') prop = 'styleFloat';
+                    if (re.test(prop)) {
+                        prop = prop.replace(re, function () {
+                            return arguments[2].toUpperCase();
+                        });
+                    }
+                    return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+                };
+                return this;
+            };
+        }
+
+        var st = window.getComputedStyle(element, null),
+            currentValue = st.getPropertyValue("-webkit-"+property) ||
+                st.getPropertyValue("-moz-"+property) ||
+                st.getPropertyValue("-ms-"+property) ||
+                st.getPropertyValue(property);
+
+        if(currentValue!== value){
+            element.parentNode.removeChild(element);
+            return false;
+        }
+        element.parentNode.removeChild(element);
+        return true;
+    });
+}
+
+Modernizr.addValueTest('transform-style', 'preserve-3d');
+
+function populateLargeLayout() {
+	var screenWidth = window.innerWidth - 14;
+	var screenHeight = window.innerHeight;
+	var widthInCards = Math.floor(screenWidth / (cardWidth + gutterWidth));
+	//$('.container').css('width', (widthInCards * (cardWidth + gutterWidth)) + 'px');
+	console.log("Width in cards: " + widthInCards);
+	console.log("Card + gutter: " + (cardWidth + gutterWidth));
+	var numCards = widthInCards * Math.floor((screenHeight + (cardHeight/1.5) + gutterWidth) / (cardHeight + gutterWidth));
+	console.log(numCards);
+	var card = (document.getElementsByClassName('card'))[3];
+	card = card.cloneNode(true);
+	card.firstChild.innerHTML = '';
+	var cardList = document.getElementById('card-list');
+
+	if(numCards - 4 > cardPhrases.length) {
+		numCards = cardPhrases.length + 4;
+	}
+
+	for(var i=0;i<numCards - 4;i++){
+		cardList.appendChild(card.cloneNode(true));
+	}
+};
 
 $(document).ready(function () {
 	var viewWidth = window.innerWidth;
@@ -307,19 +461,26 @@ $(document).ready(function () {
 	initializeCards();
 
 	if(!smallLayout) {
+		var card = $('.card')[0]
+		cardWidth = $(card).width();
+		cardHeight = $(card).height();
 		populateLargeLayout();
 		new window.Shuffler(document.getElementById('card-list'));
 		cardSlots = document.getElementsByClassName('card').length;
 		console.log('Cardslots: ' + cardSlots);
+		bindHoverFlip();
 	}
 	else {
 		cardSlots = cardPhrases.length;
 		setupSmallLayout();
-		setupHammer(document.querySelector('#active-card'));
+		var cards = document.getElementsByClassName('card');
+		for(var i = 0;i<cards.length;i++) {
+			setupHammer(cards[i]);
+		}
 	}
 	
-	output = generateRandomCards(smallLayout);
-	insertCardHTML(output);
+	outputCards = generateRandomCards(smallLayout);
+	outputCards['pointer'] = 1;
+	insertCardHTML(outputCards);
 	bindClickFlip();
-	bindHoverFlip();
 });
