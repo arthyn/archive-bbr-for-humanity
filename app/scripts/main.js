3c0130ceb552701692a67f363fc54a17bf6cbee0
Hunter Miller <hmiller@bbrcreative.com>
2014-12-16 10:54:22 -0600

    twitter share messages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -140,9 +140,14 @@ function insertCardHTML(source, load) {
 	var backs = $('.card .back');
 	for(var i = 0; i<cards.length; i++) {
 		var number = source[i+1].getImageNumber();
+		var decodeString = cardPhrases[number-1].replace(/(<([^>]+)>)/ig, ' ');
+		var twitterURL = 'https://twitter.com/home?status=' + encodeURIComponent(decodeString).replace(/'/g, escape) + '%20http://bbrforhumanity.com%20%23BBRforHumanity&source=webclient';
+		if(number-1 === 14) {twitterURL = 'https://twitter.com/home?status=I%27d%20Facebook%20like%20you%20in%20real%20life.%20http://bbrforhumanity.com%20%23BBRforHumanity&source=webclient';}
+		else if(number-1 === 4){twitterURL = 'https://twitter.com/home?status=' + encodeURIComponent(decodeString.replace(/star/g, '★')).replace(/'/g, escape) + '%20http://bbrforhumanity.com%20%23BBRforHumanity&source=webclient';}
+		console.log(decodeString);
 		$(cards[i]).empty().append(source[i+1].getHTML());
 		if(load) {
-			$(backs[i]).empty().append('<h2>Share</h2><div class="social"><span class="facebook" st_image="http://bbrforhumanity.com/images/' + number + '.png" displayText="Facebook"></span><a href="https://twitter.com/home?status=Make%20someone%27s%20day%20brighter%20with%20Holiday%20Cards%20for%20Humanity%20http://bbrforhumanity.com%20%23BBRforHumanity%20%23ForGoodnessSake%20" class="twitter" displayText="Twitter" target="_blank"></a><a href="https://pinterest.com/pin/create/button/?url=http://bbrforhumanity.com&media=http://bbrforhumanity.com/images/1.png&description=Good%20for%20goodness%20sake!" class="pinterest" displayText="Pinterest" target="_blank"></a></div>');
+			$(backs[i]).empty().append('<h2>Share</h2><div class="social"><span class="facebook" st_image="http://bbrforhumanity.com/images/' + number + '.png" displayText="Facebook"></span><a href="' + twitterURL + '" class="twitter" displayText="Twitter" target="_blank"></a><a href="https://pinterest.com/pin/create/button/?url=http://bbrforhumanity.com&media=http://bbrforhumanity.com/images/1.png&description=Good%20for%20goodness%20sake!" class="pinterest" displayText="Pinterest" target="_blank"></a></div>');
 			if(i === 3 && !smallLayout){
 				$(backs[i]).empty().append('<p>Click to download a printable deck.</p><div><a href="BBRCardsForHumanity.pdf" target="_blank"><img src="images/icon-print.png"></a></div>');
 			}
@@ -157,9 +162,9 @@ function insertCard(position){
 	var cards = $('.card');
 	var card = cards[4].cloneNode(true);
 	var num = outputCards[position].getImageNumber();
-	var decodeString = cardPhrases[num].replace(/'/g, escape).replace(/\\<*>/g, ' ');
-	console.log(decodeString);
-	var twitterURL = 'https://twitter.com/home?status=' + encodeURI(decodeString);
+	var decodeString = cardPhrases[num].replace(/(<([^>]+)>)/ig, ' ');
+	var twitterURL = 'https://twitter.com/home?status=' + encodeURI(decodeString).replace(/'/g, escape);
+	console.log()
 	$(card).find('.face').empty().append(outputCards[position].getHTML());
 	$(card).find('.back').empty().append('<h2>Share</h2><div class="social"><span class="facebook" st_image="http://bbrforhumanity.com/images/' + num + '.png" displayText="Facebook"></span><a href="https://twitter.com/home?status=Make%20someone%27s%20day%20brighter%20with%20Holiday%20Cards%20for%20Humanity%20http://bbrforhumanity.com%20%23BBRforHumanity%20%23ForGoodnessSake%20" class="twitter" displayText="Twitter" target="_blank"></a><a href="https://pinterest.com/pin/create/button/?url=http://bbrforhumanity.com&media=http://bbrforhumanity.com/images/1.png&description=Good%20for%20goodness%20sake!" class="pinterest" displayText="Pinterest" target="_blank"></a></div>')
 
